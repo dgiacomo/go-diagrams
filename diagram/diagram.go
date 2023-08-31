7620b9138e5f94cc7b9e497117995cdb131e54d9
Daniel Carbone <daniel.p.carbone@gmail.com>
2023-08-31 17:37:37 -0500

    v_v

--- diagram/diagram.go
+++ diagram/diagram.go
@@ -86,8 +86,10 @@ func (d *Diagram) Render() error {
 
 func (d *Diagram) render() error {
 	outdir := d.options.Name
-	if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
-		return err
+	if outdir != "" {
+		if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
+			return err
+		}
 	}
 
 	for _, n := range d.root.nodes {
